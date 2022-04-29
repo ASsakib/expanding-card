50b4670e771e95d118c29b53f322776d6939d2a2
Sakib <abusaeem425@gmail.com>
2022-04-29 17:55:09 +0600

    toggle added

--- asset/card.js
+++ asset/card.js
@@ -1,16 +1,7 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel)=>{
-    panel.addEventListener('click',() =>{
-        removeActiveClasses()
-        panel.classList.add('active')
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        panel.classList.toggle('active')
     })
 })
-
-
-
-const removeActiveClasses = () => {
-    panels.forEach((panel)=>{
-        panel.classList.remove('active')
-    })
-}
